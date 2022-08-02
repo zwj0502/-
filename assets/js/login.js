a081db0a600763401ff791ee757afae2e020451c
wenjie <2609082088@qq.com>
2022-08-02 10:48:14 +0800

    .

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+$('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+})
+$('#link_login').on('click', function () {
+    $('.reg-box').hide()
+    $('.login-box').show()
+})
